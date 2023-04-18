148152bd0aea39cd415945bfbfd5c0d5ffa51e19
Dario Peñaloza <dario.penaloza@syloper.com>
2023-04-18 20:39:37 -0300

    inicio del proyecto

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const songList = [
+    {
+        title: "My Cosmos is Mine",
+        file: "MyCosmosisMine.mp3",
+        cover: "MyCosmosisMine.jpeg"
+    },
+    {
+        title: "Wagging Tongue",
+        file: "WaggingTongue.mp3",
+        cover: "WaggingTongue.jpeg"
+    },
+    {
+        title: "Ghosts Again",
+        file: "GhostsAgain.mp3",
+        cover: "GhostsAgain.jpeg"
+    },
+]
