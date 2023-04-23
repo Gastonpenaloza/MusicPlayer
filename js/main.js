5229a8ca22600d8678247deee238073d02ef94f2
Dario Peñaloza <dario.penaloza@syloper.com>
2023-04-23 13:25:54 -0300

    logic to changeActiveClass function

--- js/main.js
+++ js/main.js
@@ -1,95 +1,112 @@
 const songList = [
-    {
-        title: "My Cosmos is Mine",
-        file: "MyCosmosisMine.mp3",
-        cover: "MyCosmosisMine.jpg"
-    },
-    {
-        title: "Wagging Tongue",
-        file: "WaggingTongue.mp3",
-        cover: "WaggingTongue.jpg"
-    },
-    {
-        title: "Ghosts Again",
-        file: "GhostsAgain.mp3",
-        cover: "GhostsAgain.jpg"
-    },
-    {
-        title: "Don't Say You Love Me",
-        file: "DontSayYouLoveMe.mp3",
-        cover: "DontSayYouLoveMe.jpg"
-    },
-    {
-        title: "My Favourite Stranger",
-        file: "MyFavouriteStranger.mp3",
-        cover: "MyFavouriteStranger.jpg"
-    },
-    {
-        title: "Soul With Me",
-        file: "SoulWithMe.mp3",
-        cover: "SoulWithMe.jpg"
-    },
-    {
-        title: "Caroline's Monkey",
-        file: "CarolinesMonkey.mp3",
-        cover: "CarolinesMonkey.jpg"
-    },
-    {
-        title: "Before We Drown",
-        file: "BeforeWeDrown.mp3",
-        cover: "BeforeWeDrown.jpg"
-    },
-    {
-        title: "People Are Good",
-        file: "PeopleAreGood.mp3",
-        cover: "PeopleAreGood.jpg"
-    },
-    {
-        title: "Always You",
-        file: "AlwaysYou.mp3",
-        cover: "AlwaysYou.jpg"
-    },
-    {
-        title: "Never Let Me Go",
-        file: "NeverLetMeGo.mp3",
-        cover: "NeverLetMeGo.jpg"
-    },
-    {
-        title: "Speak to Me",
-        file: "SpeaktoMe.mp3",
-        cover: "SpeaktoMe.jpg"
-    }
-]
+  {
+    title: "My Cosmos is Mine",
+    file: "MyCosmosisMine.mp3",
+    cover: "MyCosmosisMine.jpg",
+  },
+  {
+    title: "Wagging Tongue",
+    file: "WaggingTongue.mp3",
+    cover: "WaggingTongue.jpg",
+  },
+  {
+    title: "Ghosts Again",
+    file: "GhostsAgain.mp3",
+    cover: "GhostsAgain.jpg",
+  },
+  {
+    title: "Don't Say You Love Me",
+    file: "DontSayYouLoveMe.mp3",
+    cover: "DontSayYouLoveMe.jpg",
+  },
+  {
+    title: "My Favourite Stranger",
+    file: "MyFavouriteStranger.mp3",
+    cover: "MyFavouriteStranger.jpg",
+  },
+  {
+    title: "Soul With Me",
+    file: "SoulWithMe.mp3",
+    cover: "SoulWithMe.jpg",
+  },
+  {
+    title: "Caroline's Monkey",
+    file: "CarolinesMonkey.mp3",
+    cover: "CarolinesMonkey.jpg",
+  },
+  {
+    title: "Before We Drown",
+    file: "BeforeWeDrown.mp3",
+    cover: "BeforeWeDrown.jpg",
+  },
+  {
+    title: "People Are Good",
+    file: "PeopleAreGood.mp3",
+    cover: "PeopleAreGood.jpg",
+  },
+  {
+    title: "Always You",
+    file: "AlwaysYou.mp3",
+    cover: "AlwaysYou.jpg",
+  },
+  {
+    title: "Never Let Me Go",
+    file: "NeverLetMeGo.mp3",
+    cover: "NeverLetMeGo.jpg",
+  },
+  {
+    title: "Speak to Me",
+    file: "SpeaktoMe.mp3",
+    cover: "SpeaktoMe.jpg",
+  },
+];
+
+// Actual song
+
+let actualSong = null;
 
 // Capture elements of the DOM to work with JS
 
 const songs = document.getElementById("songs");
 const audio = document.getElementById("audio");
 const cover = document.getElementById("cover");
+const title = document.getElementById("title");
 
 // Load song and show list
 
 function loadSongs() {
-    songList.forEach((song, index) => { 
-        const li = document.createElement("li");
-        const link = document.createElement("a");
-        link.textContent = song.title;
-        link.href = "#";
-        link.addEventListener("click",() => loadSong(index))
-        li.appendChild(link);
-        songs.appendChild(li);
-    })
-};
+  songList.forEach((song, index) => {
+    const li = document.createElement("li");
+    const link = document.createElement("a");
+    link.textContent = song.title;
+    link.href = "#";
+    link.addEventListener("click", () => loadSong(index));
+    li.appendChild(link);
+    songs.appendChild(li);
+  });
+}
 
 // Load selected song
 
 function loadSong(songIndex) {
+  if (songIndex !== actualSong) {
+    changeActiveClass(actualSong);
+    actualSong = songIndex;
     audio.src = "./audio/" + songList[songIndex].file;
     audio.play();
-
     cover.src = "./img/" + songList[songIndex].cover;
-    
+    title.innerText = songList[songIndex].title;
+  }
 }
+
+// Change active class
+
+function changeActiveClass() {
+    const links = document.querySelectorAll("a");
+    links[actualSong].classList.remove("active");
+    links[]
+}
+
 // GO
 
 loadSongs();
